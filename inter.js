72ee2a5d6fc67e8693c094b8ae0c9df766c712f8
Laxmi Priya kannan <67217502+SanthiyaPriya2227@users.noreply.github.com>
2022-03-19 15:51:35 +0530

    Add files via upload

---
+++ inter.js
@@ -0,0 +1,90 @@
+function personal(){
+    document.getElementById("per").style.display="block";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+}
+function education(){
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="block";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+}
+
+function skills(){
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="block";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+}
+
+function certificates(){
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="block";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+
+}
+function internship(){
+
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="block";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+
+}
+function workshops(){
+
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="block";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="none";
+
+}
+function projects()
+{
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="block";
+    document.getElementById("contc").style.display="none";
+
+}
+function contacts(){
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skls").style.display="none";
+    document.getElementById("certi").style.display="none";
+    document.getElementById("intern").style.display="none";
+    document.getElementById("wshops").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("contc").style.display="block";
+
+}
